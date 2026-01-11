f7b7b8b94b1beba05c4d806610afd0cd5833ee9f
ayush422412 <ayush210yadav@gmail.com>
2026-01-12 02:33:44 +0530

    bckend changes

--- TribetalkBackend/src/index.js
+++ TribetalkBackend/src/index.js
@@ -1,14 +1,39 @@
-import dotenv from "dotenv"
-// import connectDB from "./db/index.ts";
-import {app} from './app.js'
+
+import "dotenv/config";
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+import setupGateway from "./Utils/socket/gateway.js";
+
+
 dotenv.config({
     path: './.env'
 })
 
+const app = express();
+app.use(express.json());
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running</h1>");
+});
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+setupGateway(io);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 
 // connectDB()
 // .then(() => {
